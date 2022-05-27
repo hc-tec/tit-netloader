24a6dacc5b217a33e5dbe099263b20e1c8cea612
hc-tec <2173324540@qq.com>
2022-05-27 23:38:44 +0800

    feat: use cocoyaxi as coroutine lib

--- test/core.cc
+++ test/core.cc
@@ -3,7 +3,13 @@
 //
 
 #include <iostream>
+#include <co/co.h>
 
 int main() {
+    go([]() {
+       co::sleep(1000);
+        std::cout << "run coroutine" << std::endl;
+    });
     std::cout << "hello " << std::endl;
+    co::sleep(1500);
 }
