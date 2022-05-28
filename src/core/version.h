c5d4607aef4ab70cbac65f871b71e08c418ead7c
hc-tec <2173324540@qq.com>
2022-05-28 19:01:39 +0800

    feat: add project version

---
+++ src/core/version.h
@@ -0,0 +1,10 @@
+//
+// Created by titto on 2022/5/28.
+//
+
+#ifndef TIT_NETLOADER_VERSION_H
+#define TIT_NETLOADER_VERSION_H
+
+#define TIT_NET_LOADER_VERSION "0.0.1"
+
+#endif  // TIT_NETLOADER_VERSION_H
