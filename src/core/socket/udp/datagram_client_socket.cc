494aa784bbc86c27e6130257d08e2993937ecf93
hc-tec <2173324540@qq.com>
2022-05-29 01:11:41 +0800

    feat: add udp socket

---
+++ src/core/socket/udp/datagram_client_socket.cc
@@ -0,0 +1,12 @@
+//
+// Created by titto on 2022/5/28.
+//
+
+#include "datagram_client_socket.h"
+
+namespace tit {
+namespace net {
+
+
+}  // namespace net
+}  // namespace tit
---
+++ src/core/socket/udp/datagram_client_socket.h
@@ -0,0 +1,17 @@
+//
+// Created by titto on 2022/5/28.
+//
+
+#ifndef TIT_NETLOADER_DATAGRAM_CLIENT_SOCKET_H
+#define TIT_NETLOADER_DATAGRAM_CLIENT_SOCKET_H
+
+namespace tit {
+namespace net {
+
+class datagram_client_socket {};
+
+}  // namespace net
+}  // namespace tit
+
+
+#endif  // TIT_NETLOADER_DATAGRAM_CLIENT_SOCKET_H
