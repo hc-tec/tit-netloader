2d9d9524446f51ecdfd719913d2e5dc347bf9732
hc-tec <2173324540@qq.com>
2022-05-30 15:02:23 +0800

    feat: init HostResolver

---
+++ src/core/network/host_resolver.h
@@ -0,0 +1,20 @@
+//
+// Created by titto on 2022/5/28.
+//
+
+#ifndef TIT_NETLOADER_HOST_RESOLVER_H
+#define TIT_NETLOADER_HOST_RESOLVER_H
+
+namespace tit {
+namespace net {
+
+class HostResolver {
+ public:
+
+};
+
+}  // namespace net
+}  // namespace tit
+
+
+#endif  // TIT_NETLOADER_HOST_RESOLVER_H
