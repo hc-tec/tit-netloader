6d0d6decc2878a1a402f4f55a2f0fa4a1b218af2
hc-tec <2173324540@qq.com>
2022-06-01 18:39:07 +0800

    test: core test when HTTP/1.0 is default HTTP version

--- test/core.cc
+++ test/core.cc
@@ -19,16 +19,16 @@ int main() {
 
     net::NetworkService* service = net::GetNetworkService();
     net::RequestParams params;
-    params.request_info.url = net::URL("https://127.0.0.1:443/hello");
-    params.request_info.method = net::Method::POST;
+    params.request_info.url = net::URL("https://blog.csdn.net/Emperor10/article/details/123179056");
+    params.request_info.method = net::Method::GET;
     net::HttpRequestHeaders& headers = params.request_info.headers;
 
-//    headers.PutHeaders("accept-encoding", "gzip, deflate, br");
+    headers.PutHeaders("accept-encoding", "gzip, deflate, br");
     headers.PutHeaders("accept", "*/*");
 //    headers.PutHeaders(net::HttpHeaders::CONNECTION,
 //                       net::HttpHeaders::Value::CONNECTION_KEEP_ALIVE);
-//    headers.PutHeaders("host", params.request_info.url.host());
-//    headers.PutHeaders("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53");
+    headers.PutHeaders("host", params.request_info.url.host());
+    headers.PutHeaders("user-agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.53");
 
 //    headers.PutHeaders("upgrade-insecure-requests","1");
 //    headers.PutHeaders(":scheme","https");
@@ -44,8 +44,8 @@ int main() {
 //    headers.PutHeaders("content-type","/p/133375078");
 //    headers.PutHeaders("referer","https://github.com/");
 
-    params.request_info.body = std::make_shared<net::HttpRequestBufferBody>(
-        "application/json;charset=UTF-8", "ping");
+//    params.request_info.body = std::make_shared<net::HttpRequestBufferBody>(
+//        "application/json;charset=UTF-8", "ping");
     std::unique_ptr<net::URLLoader> loader = service->CreateURLLoader(params);
     loader->Start();
   });
